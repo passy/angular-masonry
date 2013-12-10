5d294c384a0719fc7a46e2fc108d512a665a1deb
James Arthur <thruflo@gmail.com>
2013-12-10 14:54:17 +0000

    refacttor: whitespace and make image loaded call optional.

--- src/angular-masonry.js
+++ src/angular-masonry.js
@@ -5,108 +5,96 @@
  */
 (function () {
   'use strict';
-
-  angular.module('wu.masonry', [])
-    .controller('MasonryCtrl', function controller($scope, $element, $timeout) {
+  angular.module('wu.masonry', [
+  ]).controller('MasonryCtrl', function controller ($scope, $element, $timeout) {
       var bricks = {};
       var schedule = [];
       var destroyed = false;
       var self = this;
       var timeout = null;
-
       this.scheduleMasonryOnce = function scheduleMasonryOnce() {
         var args = arguments;
-        var found = schedule.filter(function filterFn(item) {
-          return item[0] === args[0];
-        }).length > 0;
-
+        var found = schedule.filter(
+          function filterFn (item) {
+            return item[0] === args[0];
+          }
+        ).length > 0;
         if (!found) {
           this.scheduleMasonry.apply(null, arguments);
         }
       };
-
-      // Make sure it's only executed once within a reasonable time-frame in
-      // case multiple elements are removed or added at once.
-      this.scheduleMasonry = function scheduleMasonry() {
+      this.scheduleMasonry = function scheduleMasonry () {
         if (timeout) {
           $timeout.cancel(timeout);
         }
-
         schedule.push([].slice.call(arguments));
-
-        timeout = $timeout(function runMasonry() {
+        timeout = $timeout(
+          function runMasonry() {
+            if (destroyed) {
+              return;
+            }
+            schedule.forEach(
+              function scheduleForEach (args) {
+                $element.masonry.apply($element, args);
+              }
+            );
+            schedule = [];
+          }, 
+          60
+        );
+      };
+      this.appendBrick = function appendBrick (element, id) {
+        function _append () {
           if (destroyed) {
             return;
           }
-          schedule.forEach(function scheduleForEach(args) {
-            $element.masonry.apply($element, args);
-          });
-          schedule = [];
-        }, 30);
-      };
-
-      function defaultLoaded($element) {
-        $element.addClass('loaded');
-        $scope.$emit('masonry.imagesLoaded', $element);
-      }
-
-      this.appendBrick = function appendBrick(element, id) {
-        if (destroyed) {
-          return;
-        }
-
-        function _append() {
           if (Object.keys(bricks).length === 0) {
             $element.masonry('resize');
           }
-
           if (bricks[id] === undefined) {
-            // I wanted to make this dynamic but ran into huuuge memory leaks
-            // that I couldn't fix. If you know how to dynamically add a
-            // callback so one could say <masonry loaded="callback($element)">
-            // please submit a pull request!
-            defaultLoaded(element);
-
-            // Keep track of added elements.
             bricks[id] = true;
             $element.masonry('appended', element, true);
             self.scheduleMasonryOnce('reloadItems');
             self.scheduleMasonryOnce('layout');
           }
+        };
+        function _loaded () {
+          element.addClass('loaded');
+          $scope.$emit('masonry.imagesLoaded', element);
+        };
+        var has_fixed_dimension = element.find('[ng-img-preload-aspect-ratio]').length;
+        if (has_fixed_dimension) {
+          _append();
+          element.imagesLoaded(_loaded);
         }
-
-        element.imagesLoaded(_append);
+        else {
+          element.imagesLoaded(function () {
+            _append();
+            _loaded();
+          });
+        }
       };
-
       this.removeBrick = function removeBrick(id, element) {
         if (destroyed) {
           return;
         }
-
         delete bricks[id];
         $element.masonry('remove', element);
         this.scheduleMasonryOnce('layout');
       };
-
       this.destroy = function destroy() {
-        destroyed = true;
-
         if ($element.data('masonry')) {
-          // Gently uninitialize if still present
           $element.masonry('destroy');
         }
         $scope.$emit('masonry.destroyed');
-
         bricks = [];
+        destroyed = true;
       };
-
       this.reload = function reload() {
         $element.masonry();
         $scope.$emit('masonry.reloaded');
       };
-
-
-    }).directive('masonry', function masonryDirective() {
+  }).directive('masonry', function masonryDirective () {
       return {
         restrict: 'AE',
         controller: 'MasonryCtrl',
@@ -118,7 +106,6 @@
               columnWidth: parseInt(attrs.columnWidth, 10)
             });
             element.masonry(options);
-
             scope.$emit('masonry.created', element);
             scope.$on('masonry.reload', function () {
               ctrl.reload();
@@ -127,20 +114,19 @@
           }
         }
       };
-    }).directive('masonryBrick', function masonryBrickDirective() {
+  }).directive('masonryBrick', function masonryBrickDirective () {
       return {
         restrict: 'AC',
         require: '^masonry',
         scope: true,
         link: {
           pre: function preLink(scope, element, attrs, ctrl) {
-            var id = scope.$id, index;
-
+            var id = scope.$id
+            var index;
             ctrl.appendBrick(element, id);
             element.on('$destroy', function () {
               ctrl.removeBrick(id, element);
             });
-
             scope.$watch('$index', function () {
               if (index !== undefined && index !== scope.$index) {
                 ctrl.scheduleMasonryOnce('reloadItems');
@@ -151,5 +137,5 @@
           }
         }
       };
-    });
-}());
+  });
+})();
